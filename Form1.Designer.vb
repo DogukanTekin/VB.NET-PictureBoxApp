6ff870a48c260c9fdf831d02d12865413768168e
DogukanTekin <95180909+DogukanTekin@users.noreply.github.com>
2022-04-06 02:03:59 +0300

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 99)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(665, 553)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(168, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(257, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "MOUSE İLE ÜZERİNDE GEZERKEN ÇİZGİ ÇEKME"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(723, 99)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(665, 553)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(946, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(238, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "TIKLANAN 2 NOKTA ARASINDA ÇİZGİ ÇEKME"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1435, 664)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Dim x1, x2, y1, y2, sayac As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.Image = Image.FromFile(Application.StartupPath + "\pictureodev_siyah.jpg")
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.Image = Image.FromFile(Application.StartupPath + "\pictureodev_siyah.jpg")
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(sender As Object, e As MouseEventArgs) Handles PictureBox1.MouseMove
+        PictureBox1.CreateGraphics.DrawLine(Pens.Yellow, e.X, e.Y, e.X + 1, e.Y + 1)
+    End Sub
+
+    Private Sub PictureBox2_MouseClick(sender As Object, e As MouseEventArgs) Handles PictureBox2.MouseClick
+        sayac += 1
+        'Doğru çalışan ama çizgiyi 2 adımda bir tersten çeken kodlar
+        'If (sayac = 1) Then
+        '    x1 = e.X
+        '    y1 = e.Y
+        'ElseIf (sayac = 2) Then
+        '    x2 = e.X
+        '    y2 = e.Y
+        '    PictureBox2.CreateGraphics.DrawLine(Pens.Red, x1, y1, x2, y2)
+        '    x1 = x2
+        '    y1 = y2
+        'Else
+        '    If (sayac Mod 2 = 1) Then
+        '        x1 = e.X
+        '        y1 = e.Y
+        '    Else
+        '        x2 = e.X
+        '        y2 = e.Y
+        '    End If
+        '    PictureBox2.CreateGraphics.DrawLine(Pens.Red, x1, y1, x2, y2)
+        'End If
+
+        'Çizgiyi önceki tıklanmış yerden şu an tıklanan yere çeken program
+        If sayac = 1 Then
+            x1 = e.X
+            y1 = e.Y
+        Else
+            x2 = e.X
+            y2 = e.Y
+        End If
+        If sayac > 1 Then
+            PictureBox2.CreateGraphics.DrawLine(Pens.Red, x1, y1, x2, y2)
+            x1 = x2
+            y1 = y2
+        End If
+    End Sub
+End Class
